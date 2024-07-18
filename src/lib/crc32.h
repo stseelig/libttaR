2039016650bbc6071e270e6f8606d7c62544b145
stseelig <stseelig@mail.com>
2024-07-18 00:37:22 -0400

    comment

--- src/lib/crc32.h
+++ src/lib/crc32.h
@@ -38,9 +38,9 @@ crc32_cont(const u8 x, const u32 crc)
 /*@*/
 {
 	// having the variable instead of just having a one line return is
-	//   important. otherwise it does a xor against memory, which can be
-	//   much slower (I think it has something to do with dependency
-	//   chains and pipelining). probably a compiler bug
+	//   important. otherwise it does (on x86) a xor against memory, which
+	//   can be much slower (I think it has something to do with
+	//   dependency chains and pipelining). probably a compiler bug
 	register const u32 lookup = crc32_table[((u8) crc) ^ x];
 	return (u32) ((crc >> 8u) ^ lookup);
 }
