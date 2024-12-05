387aad8bc34932279ca98b83a6efc1e54ec1e241
stseelig <stseelig@mail.com>
2024-12-05 13:02:39 -0500

    update for prev. commit

--- src/version.h
+++ src/version.h
@@ -27,9 +27,9 @@
 #define CLI_VERSION_NUM		1u
 #define CLI_VERSION_NUM_MAJOR	1u
 #define CLI_VERSION_NUM_MINOR	7u
-#define CLI_VERSION_NUM_REVIS	3u
+#define CLI_VERSION_NUM_REVIS	4u
 #define CLI_VERSION_STR_EXTRA	""
-#define CLI_VERSION_STR_DATE	"2024-12-03"
+#define CLI_VERSION_STR_DATE	"2024-12-05"
 #define CLI_COPYRIGHT_STR	\
 	"Copyright (C) 2023-2024, Shane Seelig"
 #define CLI_LICENSE_STR		"GPL-3.0-or-later"
